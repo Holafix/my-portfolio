c1230dfa999c850046eb5943f5a15f99f0bf61b7
Holafix <olatundeolawale111@gmail.com>
2025-02-24 09:48:49 +0100

    updated

--- src/data/constants.js
+++ src/data/constants.js
@@ -13,7 +13,7 @@ export const Bio = {
     "I am a motivated, self learned and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/Holafix",
   resume:
-    "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
+    "https://drive.google.com/file/d/1UiLhANWO2NWvLkga4SEXdweQU-E8VAzP/view?usp=drive_link",
   linkedin: "https://www.linkedin.com/in/holatunde",
   twitter: "https://twitter.com/holatundeholaw5",
   insta: "https://www.instagram.com/holafix1",
